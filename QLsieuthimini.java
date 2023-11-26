b1cec27e21ec70fedb473810aade5a709212fa55
Kietnehi <149951917+Kietnehi@users.noreply.github.com>
2023-11-26 23:24:21 +0700

    sửa  hàm main lại một chút

--- QLsieuthimini.java
+++ QLsieuthimini.java
@@ -14,23 +14,31 @@ public class QLsieuthimini {
             System.out.println("3. Quản lý Khách hàng");
             System.out.println("4. Quản lý Sản phẩm");
             System.out.println("5. Quản lý Hóa đơn");
-
+                    System.out.println("6. Thoát");
 			switch(select) {
 			case 1:
 				QLNhaCungCap ql1 = new QLNhaCungCap();
 				ql1.menu();
-			case 2:
+                                break;
+                        case 2:
 				QLNhanVien ql2 = new QLNhanVien();
 				ql2.menu();
+                                break;
 			case 3:
 				QLKhachHang ql3 = new QLKhachHang();
 				ql3.menu();
+                                break;
 			case 4:
 				QLSanPham ql4 = new QLSanPham();
 				ql4.menu();
+                                break;
 			case 5:
 				QLHoaDon ql5 = new QLHoaDon();
 				ql5.menu();
+                                break;
+                        case 6: 
+                            System.out.println("Chương trình đã dừng");
+                            break;
 			default:
 	            System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
 			}
